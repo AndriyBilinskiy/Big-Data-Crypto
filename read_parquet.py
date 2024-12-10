8ad4e1d4425d74c89d6119c5319456748228420e
Ivan Varanytsia <ivan.varanytsia@ucu.edu.ua>
2024-12-10 22:07:36 +0200

    fix reading parquet file

--- read_parquet.py
+++ read_parquet.py
@@ -1,13 +1,9 @@
 from pyspark.sql import SparkSession
 
-# Initialize Spark session
 spark = SparkSession.builder \
     .appName("Read Parquet Data") \
     .getOrCreate()
 
-# Read the Parquet data
-parquet_df = spark.read.parquet("data")
-
-# Show the data
+parquet_df = spark.read.parquet("data/part-*.parquet")
 parquet_df.show()
 print(parquet_df.count())
